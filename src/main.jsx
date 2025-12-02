7be8dc7ee5abb5253484e5b0cc2c37557eb7a25d
jito8web <randria.jac4@gmail.com>
2025-12-02 14:36:44 +0300

    Fix BrowserRouter basename path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Mathfi">
+    <BrowserRouter basename="/Mathfi/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
