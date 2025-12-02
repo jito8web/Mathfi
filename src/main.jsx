f2182d21844552237122a636e31e56d9ba747592
jito8web <randria.jac4@gmail.com>
2025-12-02 14:45:22 +0300

    Remove basename from BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Mathfi/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
